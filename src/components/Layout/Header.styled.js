e9a92808d4d91503397c09660d2e115314b769a5
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:11 +0200

    Update Header.styled.js

--- src/components/Layout/Header.styled.js
+++ src/components/Layout/Header.styled.js
@@ -5,7 +5,7 @@ export const HeaderContainer = styled.div`
   padding: ${p => p.theme.space[4]}px;
   display: flex;
   align-items: center;
-  gap: ${p => p.theme.space[5]}px;
+  gap: ${p => p.theme.space[6]}px;
 
   min-width: 400px;
   max-width: 1200px;
@@ -14,7 +14,8 @@ export const HeaderContainer = styled.div`
 export const Navigation = styled.nav`
   margin-left: auto;
   display: flex;
-  gap: ${p => p.theme.space[4]}px;
+  gap: ${p => p.theme.space[5]}px;
+  color: ${p => p.theme.colors.black};
 
   font-size: ${p => p.theme.fontSizes.m};
   a {
@@ -22,6 +23,47 @@ export const Navigation = styled.nav`
   }
   .active {
     padding: ${p => p.theme.space[2]}px 0;
+    color: ${p => p.theme.colors.white};
     border-bottom: 2px solid ${p => p.theme.colors.white};
+  }
+`;
+
+export const MenuUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: ${p => p.theme.space[3]}px;
+
+  img {
+    border-radius: ${p => p.theme.radii.round};
+  }
+`;
+
+export const Button = styled.button`
+  padding: ${p => p.theme.space[3]}px ${p => p.theme.space[2]}px;
+  width: 90px;
+  height: ${p => p.theme.space[5]}px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: ${p => p.theme.fontSizes.m};
+  line-height: ${p => p.theme.lineHeights.body}px;
+  text-align: center;
+
+  background-color: ${p => p.theme.colors.white};
+  color: ${p => p.theme.colors.accent};
+  border-radius: ${p => p.theme.space[2]}px;
+  border: ${p => p.theme.space[1]}px solid ${p => p.theme.colors.accent};
+  outline: none;
+
+  transition: border-color ${p => p.theme.transition},
+    color ${p => p.theme.transition};
+
+  &:hover,
+  &:focus {
+    border-color: ${p => p.theme.colors.black};
+    color: ${p => p.theme.colors.black};
   }
 `;
