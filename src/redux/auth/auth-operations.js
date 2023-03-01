1b2edc67d435940c0fd9978ee466c66d887b2550
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:30 +0200

    Create auth-operations.js

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const register = createAsyncThunk('auth/register', async credentials => {
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    // return thunkApi.rejectWithValue(error.message);
+  }
+});
+
+export const logIn = createAsyncThunk('auth/login', async credentials => {
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    // return thunkApi.rejectWithValue(error.message);
+  }
+});
+
+export const logOut = createAsyncThunk('auth/logout', async () => {
+  try {
+    const { data } = await axios.post('/users/logout');
+    token.unset();
+    return data;
+  } catch (error) {
+    // return thunkApi.rejectWithValue(error.message);
+  }
+});
+
+// Lou1
+// FLS_001@mail.com
