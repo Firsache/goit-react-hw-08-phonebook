f8948d5e1086bf872746a772623c65611c4349e4
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:26 +0200

    Update SignUp.jsx

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,42 +1,63 @@
-import { BsEnvelope, BsLock } from 'react-icons/bs';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BsEnvelope, BsPersonPlus, BsKey } from 'react-icons/bs';
+
+import { register } from 'redux/auth/auth-operations';
+import { Container } from 'common/common.styled';
 import {
   Button,
   Input,
   Label,
   Span,
 } from 'components/ContactForm/ContactForm.styled';
-import { useState } from 'react';
-import { Container } from 'common/common.styled';
 
-const SignUpForm = () => {
-  //   const dispatch = useDispatch();
-
+const RegisterForm = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
   const handleInputChange = evt => {
     const { name, value } = evt.target;
 
+    if (name === 'name') setName(value);
     if (name === 'email') setEmail(value);
     if (name === 'password') setPassword(value);
   };
 
   const handleSubmit = evt => {
     evt.preventDefault();
-    //     dispatch(getSignUpFrom({ email, password }));
-    //     setEmail('');
-    //   setPassword('');
+    dispatch(register({ name: name.trim(), email, password }));
+
+    setName('');
+    setEmail('');
+    setPassword('');
   };
 
   return (
     <Container>
       <form onSubmit={handleSubmit}>
         <Label>
+          <Span>Name</Span>
+          <BsPersonPlus size={15} className="icon" />
+          <Input
+            type="text"
+            name="name"
+            minLength={3}
+            maxLength={20}
+            required
+            value={name}
+            onChange={handleInputChange}
+          />
+        </Label>
+        <Label>
           <Span>Email</Span>
           <BsEnvelope size={15} className="icon" />
           <Input
             type="email"
             name="email"
+            minLength={3}
+            maxLength={30}
             required
             value={email}
             onChange={handleInputChange}
@@ -44,10 +65,12 @@ const SignUpForm = () => {
         </Label>
         <Label>
           <Span>Password</Span>
-          <BsLock size={15} className="icon" />
+          <BsKey size={15} className="icon" />
           <Input
             type="password"
             name="password"
+            minLength={7}
+            maxLength={20}
             required
             value={password}
             onChange={handleInputChange}
@@ -59,4 +82,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default RegisterForm;
