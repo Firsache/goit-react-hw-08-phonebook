8d4c665c4a8de056866359a6366bf0bbb78d1c8b
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-01 21:56:18 +0200

    Update ThemeSwitcher.styled.js

--- src/components/ThemeSwitcher/ThemeSwitcher.styled.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Switcher = styled.button`
-  position: absolute;
+  /* position: absolute;
   top: ${p => p.theme.space[4]}px;
-  right: ${p => p.theme.space[6]}px;
+  right: ${p => p.theme.space[6]}px; */
   color: ${p => p.theme.colors.white};
 `;
