1a3d0c44da766372fdfda856f1318ce48e09cef4
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:08 +0200

    Update Header.jsx

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,17 @@
-import { Title } from 'common/common.styled';
-import { Box, ThemeSwitcher } from 'components';
-import { routes } from 'helpers/routes';
+import { useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import { MdContactPhone } from 'react-icons/md';
+
+import { selectedIsLoggedIn } from 'redux/auth/selectors';
+import { routes } from 'helpers/routes';
+
+import { Title } from 'common/common.styled';
+import { Box, ThemeSwitcher, UserMenu } from 'components';
 import { HeaderContainer, Navigation } from './Header.styled';
 
 export const Header = ({ normalizedTheme }) => {
+  const isLoggedIn = useSelector(selectedIsLoggedIn);
+
   return (
     <Box bg={normalizedTheme.colors.accent} as="header">
       <HeaderContainer>
@@ -23,10 +29,17 @@ export const Header = ({ normalizedTheme }) => {
         </Link>
 
         <Navigation>
-          <NavLink to={routes.CONTACTS}>Contacts</NavLink>
-          <NavLink to={routes.LOGIN}>Sign In</NavLink>
-          <NavLink to={routes.REGISTER}>Sign Up</NavLink>
+          {isLoggedIn ? (
+            <NavLink to={routes.CONTACTS}>Contacts</NavLink>
+          ) : (
+            <>
+              <NavLink to={routes.REGISTER}>Register</NavLink>
+              <NavLink to={routes.LOGIN}>Sign In</NavLink>
+            </>
+          )}
         </Navigation>
+
+        {isLoggedIn && <UserMenu />}
 
         <ThemeSwitcher />
       </HeaderContainer>
