9d8f5dc9d178ee2bd1ec4a73479673f451ca8350
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-01 21:56:16 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,12 @@
 import { Box } from 'components';
 import { FallingLines } from 'react-loader-spinner';
+import { theme } from 'styles/theme';
 
-export const Loader = ({ themeNorm }) => {
+export const Loader = () => {
   return (
     <Box position="absolute" top="50%" left="50%">
       <FallingLines
-        color={themeNorm.colors.accent}
+        color={theme.color.accent}
         width="100"
         visible={true}
         ariaLabel="falling-lines-loading"
