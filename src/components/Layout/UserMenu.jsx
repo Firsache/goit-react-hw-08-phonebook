0138d310ab2eda6175c1ad98424c23148dc00d23
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:14 +0200

    Create UserMenu.jsx

---
+++ src/components/Layout/UserMenu.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedUserName } from 'redux/auth/selectors';
+
+import { logOut } from 'redux/auth/auth-operations';
+import defaultPicture from 'img/avatar.svg';
+import { MenuUser, Button } from './Header.styled';
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectedUserName);
+  const avatar = defaultPicture;
+
+  const handleLogOutClick = () => {
+    dispatch(logOut());
+  };
+
+  return (
+    <MenuUser>
+      <img src={avatar} alt={userName} width={23} />
+      <div>Welcome, {userName}</div>
+      <Button onClick={handleLogOutClick}>Log out</Button>
+    </MenuUser>
+  );
+};
