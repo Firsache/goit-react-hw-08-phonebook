fb4e6351b099272eaf825d500df4b0a54878a5bc
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:38 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -7,9 +7,6 @@ import {
 
 const initialContactsState = {
   contacts: [],
-  isLoading: false,
-  error: null,
-
   filteredName: '',
 };
 
@@ -23,42 +20,19 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      // get contact
-      .addCase(getContacts.pending, pendingHandler)
       .addCase(getContacts.fulfilled, (state, { payload }) => {
-        state.isLoading = false;
         state.contacts = payload;
       })
-      .addCase(getContacts.rejected, errorHandler)
-      // add contact
-      .addCase(addContacts.pending, pendingHandler)
       .addCase(addContacts.fulfilled, (state, { payload }) => {
-        state.isLoading = false;
         state.contacts = [...state.contacts, payload];
       })
-      .addCase(addContacts.rejected, errorHandler)
-      // delete contact
-
-      .addCase(delContacts.pending, pendingHandler)
       .addCase(delContacts.fulfilled, (state, { payload }) => {
-        state.isLoading = false;
         state.contacts = state.contacts.filter(
           contact => contact.id !== payload
         );
-      })
-      .addCase(delContacts.rejected, errorHandler);
+      });
   },
 });
-
-function pendingHandler(state) {
-  state.isLoading = true;
-  state.error = null;
-}
-
-function errorHandler(state, { payload }) {
-  state.isLoading = false;
-  state.error = payload;
-}
 
 export const { setFilteredName } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
