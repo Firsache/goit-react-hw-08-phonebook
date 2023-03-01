af6f626bcca3bccbcb67d0f27c43a27b0f5d566c
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 01:20:17 +0200

    Update contacts-api.js

--- src/helpers/contacts-api.js
+++ src/helpers/contacts-api.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://63fcc2cd677c415873133e1e.mockapi.io/api/v1';
+// axios.defaults.baseURL = 'https://63fcc2cd677c415873133e1e.mockapi.io/api/v1';
 
-export const fetchContacts = async () => {
-  const { data } = await axios.get('/contacts');
-  return data;
-};
+// export const fetchContacts = async () => {
+//   const { data } = await axios.get('/contacts');
+//   return data;
+// };
 
-export const postContacts = async contact => {
-  const { data } = await axios.post('/contacts', contact);
-  return data;
-};
+// export const postContacts = async contact => {
+//   const { data } = await axios.post('/contacts', contact);
+//   return data;
+// };
 
-export const deleteContact = async contactId => {
-  const { data } = await axios.delete(`/contacts/${contactId}`);
-  return data;
-};
+// export const deleteContact = async contactId => {
+//   const { data } = await axios.delete(`/contacts/${contactId}`);
+//   return data;
+// };
