9f255e895ab7b9674bf87dfdfaae7ce83d09c384
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 16:47:27 +0200

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,3 +2,4 @@ export const selectContacts = state => state.contacts.contacts;
 export const selectLoader = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilteredName = state => state.contacts.filteredName;
+export const selectEditModal = state => state.contacts.showModal;
