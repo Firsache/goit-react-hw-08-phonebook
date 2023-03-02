68bc93973fd2fef00c3d73bb9a9e1728d9b28dea
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 13:35:54 +0200

    Update UserMenu.jsx

--- src/components/Layout/UserMenu.jsx
+++ src/components/Layout/UserMenu.jsx
@@ -1,25 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectedIsLoggedIn, selectedUserName } from 'redux/auth/selectors';
+import { selectedUserName } from 'redux/auth/selectors';
 
 import { logOut } from 'redux/auth/auth-operations';
 import defaultPicture from 'img/avatar.svg';
 import { MenuUser, Button } from './Header.styled';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { routes } from 'helpers/routes';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const userName = useSelector(selectedUserName);
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector(selectedIsLoggedIn);
-
   const avatar = defaultPicture;
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate(routes.LOGIN);
-    }
-  }, [isLoggedIn, navigate]);
 
   const handleLogOutClick = () => {
     dispatch(logOut());
