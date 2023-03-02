a267836dff7fb9dede2aff5d5a9e20e995166423
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 11:33:36 +0200

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -13,27 +13,12 @@ const initialAuthState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState: initialAuthState,
-  reducers: {
-    // setFilteredName(state, { payload }) {
-    //   state.filteredName = payload;
-    // },
-  },
   extraReducers: builder => {
     builder
-      //   .addCase(register.fulfilled, (state, { payload }) => {
-      //     state.user = payload.user;
-      //     state.token = payload.token;
-      //     state.isLoggedIn = true;
-      //   })
-
-      //   .addCase(logIn.fulfilled, (state, { payload }) => {
-      //     state.user = payload;
-      //     state.token = payload.token;
-      //     state.isLoggedIn = true;
-      //   })
-
       .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
         state.isLoggedIn = false;
+        state.token = null;
       })
       .addMatcher(isAnyOf(...getActions('fulfilled')), successLogInHandler);
   },
@@ -45,5 +30,4 @@ function successLogInHandler(state, { payload }) {
   state.isLoggedIn = true;
 }
 
-// export const { setFilteredName } = authSlice.actions;
 export const authReducer = authSlice.reducer;
