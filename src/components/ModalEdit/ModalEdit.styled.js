5795479ac1ae3f6920da8cf85bf9f9a2e3c6bbfa
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 22:24:53 +0200

    Update ModalEdit.styled.js

--- src/components/ModalEdit/ModalEdit.styled.js
+++ src/components/ModalEdit/ModalEdit.styled.js
@@ -14,6 +14,25 @@ export const Backdrop = styled.div`
 `;
 
 export const Modal = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-width: 350px;
+  width: 100%;
+  min-height: 150px;
+  padding: 40px;
+
+  background-color: #f7f7f7;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+
+  transform: translate(-50%, -50%) scaleY(1);
+`;
+
+export const CloseBtn = styled.button`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  padding: 6px;
 `;
