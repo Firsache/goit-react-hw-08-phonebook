f95161d6dc83bc726b95b64b7a46132aa799590d
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 11:33:33 +0200

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -12,35 +12,44 @@ const token = {
   },
 };
 
-export const register = createAsyncThunk('auth/register', async credentials => {
-  try {
-    const { data } = await axios.post('/users/signup', credentials);
-    token.set(data.token);
-    return data;
-  } catch (error) {
-    // return thunkApi.rejectWithValue(error.message);
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-});
+);
 
-export const logIn = createAsyncThunk('auth/login', async credentials => {
-  try {
-    const { data } = await axios.post('/users/login', credentials);
-    token.set(data.token);
-    return data;
-  } catch (error) {
-    // return thunkApi.rejectWithValue(error.message);
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/login', credentials);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-});
+);
 
-export const logOut = createAsyncThunk('auth/logout', async () => {
-  try {
-    const { data } = await axios.post('/users/logout');
-    token.unset();
-    return data;
-  } catch (error) {
-    // return thunkApi.rejectWithValue(error.message);
+export const logOut = createAsyncThunk(
+  'auth/logout',
+  async ({ rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/logout');
+      token.unset();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-});
+);
 
 // Lou1
 // FLS_001@mail.com
