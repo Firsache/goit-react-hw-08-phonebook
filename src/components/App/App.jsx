553e6c5037855f13b44366cfaeb878e09fd26593
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 13:35:52 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,11 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { routes } from 'helpers/routes';
 import { Layout, Loader } from '../index';
+import { selectToken } from 'redux/auth/selectors';
+import { getCurrentUser } from 'redux/auth/auth-operations';
 
 const HomePage = lazy(() => import('pages/Homepage/Homepage'));
 const ContactsPage = lazy(() => import('pages/Contacts/Contacts'));
@@ -10,6 +13,16 @@ const SignUpFormPage = lazy(() => import('pages/SignUp/SignUp'));
 const SignInFormPage = lazy(() => import('pages/SignIn/SignIn'));
 
 export function App() {
+  const token = useSelector(selectToken);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(token);
+
+    if (!token) return;
+
+    dispatch(getCurrentUser());
+  }, [dispatch, token]);
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
