d0449a1fed063a5463a08c7d9e0e9ae4be86859c
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 22:53:14 +0200

    Update Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,27 @@
+import { Container } from 'common/common.styled';
+import { routes } from 'helpers/routes';
+
+import { Main, StyledLink, AppTitle } from './Homepage.styled';
+
 const HomePage = () => {
-  return <>Hello and welcome!</>;
+  return (
+    <Container>
+      <h1>Welcome to your Phonebook!</h1>
+
+      <Main>
+        <p>
+          Searched for a place where you can keep and edit your contacts? You've
+          found it! Test it yourself, we're sure you'll love our app!
+        </p>
+        <p>
+          Follow the links{' '}
+          <StyledLink to={routes.REGISTER}>Register</StyledLink> and{' '}
+          <StyledLink to={routes.LOGIN}>Log in</StyledLink> and enjoy your{' '}
+          <AppTitle>Phonebook</AppTitle>!
+        </p>
+      </Main>
+    </Container>
+  );
 };
 
 export default HomePage;
