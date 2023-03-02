0f1479b85714731fc3d1f5e9528e8cf5c7368bec
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 23:30:14 +0200

    Update ModalEdit.styled.js

--- src/components/ModalEdit/ModalEdit.styled.js
+++ src/components/ModalEdit/ModalEdit.styled.js
@@ -22,7 +22,7 @@ export const Modal = styled.div`
   min-height: 150px;
   padding: 40px;
 
-  background-color: #f7f7f7;
+  background-color: ${p => p.theme.colors.backgroundColorModal};
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
     0px 2px 1px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
