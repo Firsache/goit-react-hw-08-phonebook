a04241944cac2f0458d391435c70e377f4ced3d8
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-02 14:36:43 +0200

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,3 +1,2 @@
 export const selectedUserName = state => state.auth.user.name;
 export const selectedIsLoggedIn = state => state.auth.isLoggedIn;
-export const selectToken = state => state.auth.token;
