d59f35a8aae6941ac83ec0876cca7aecae4c9b1c
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-03 13:08:20 +0200

    Update UserMenu.jsx

--- src/components/Layout/UserMenu.jsx
+++ src/components/Layout/UserMenu.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedUserName } from 'redux/auth/selectors';
+import { useMediaQuery } from 'react-responsive';
 
 import { logOut } from 'redux/auth/auth-operations';
 import defaultPicture from 'img/avatar.svg';
@@ -10,15 +11,16 @@ export const UserMenu = () => {
   const userName = useSelector(selectedUserName);
   const avatar = defaultPicture;
 
+  const isDesktop = useMediaQuery({ minWidth: 767 });
+
   const handleLogOutClick = () => {
-    // console.log('click');
     dispatch(logOut());
   };
 
   return (
     <MenuUser>
       <img src={avatar} alt={userName} width={23} />
-      <div>Welcome, {userName}</div>
+      {isDesktop && <div>Welcome, {userName}</div>}
       <Button onClick={handleLogOutClick}>Log out</Button>
     </MenuUser>
   );
