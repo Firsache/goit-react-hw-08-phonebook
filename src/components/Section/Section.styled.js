ebcf5a5dcf2805bc9b0566b1a5b17b6dca6a67cd
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-03 13:08:22 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -12,8 +12,11 @@ export const Wrapper = styled.section`
 export const Title = styled.h2`
   padding-bottom: ${p => p.theme.space[3]}px;
   font-weight: ${p => p.theme.fontWeights.bold};
-  font-size: ${p => p.theme.fontSizes.l};
+  font-size: 26px;
 
   text-shadow: 3px 3px 3px ${p => p.theme.colors.accent};
   text-align: ${({ titlePosition }) => titlePosition};
+  @media screen and (min-width: 480px) {
+    font-size: ${p => p.theme.fontSizes.l};
+  }
 `;
