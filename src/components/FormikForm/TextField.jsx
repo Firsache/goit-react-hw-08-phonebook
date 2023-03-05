f9cf8d31297721fb09a53d2e6450ac1e5771db30
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-05 13:48:21 +0200

    Update TextField.jsx

--- src/components/FormikForm/TextField.jsx
+++ src/components/FormikForm/TextField.jsx
@@ -1,13 +1,21 @@
 import { Label, Span, Input } from 'components/ContactForm/ContactForm.styled';
 import { useField } from 'formik';
-// import { BsEnvelope, BsKey } from 'react-icons/bs';
+import {
+  BsEnvelope,
+  BsKey,
+  BsTelephonePlus,
+  BsPersonPlus,
+} from 'react-icons/bs';
 
 export const TextField = ({ label, ...otherProps }) => {
   const [field, meta] = useField(otherProps);
   return (
     <Label>
       <Span>{label}</Span>
-      {/* <BsEnvelope size={15} className="icon" /> */}
+      {label === 'Name' && <BsPersonPlus size={15} className="icon" />}
+      {label === 'Email' && <BsEnvelope size={15} className="icon" />}
+      {label === 'Password' && <BsKey size={15} className="icon" />}
+      {label === 'Number' && <BsTelephonePlus size={15} className="icon" />}
       <Input {...field} {...otherProps} />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
